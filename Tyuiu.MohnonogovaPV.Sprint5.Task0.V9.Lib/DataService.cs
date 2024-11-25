59e3960814b87769a59a8bb88e9d2fcb8f48faaf
Tyuiu.MohnonogovaPV@outlook.com <Tyuiu.MohnonogovaPV@outlook.com>
2024-11-25 12:50:13 +0500

    Task0

--- Tyuiu.MohnonogovaPV.Sprint5.Task0.V9.Lib/DataService.cs
+++ Tyuiu.MohnonogovaPV.Sprint5.Task0.V9.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.MohnonogovaPV.Sprint5.Task0.V9.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask0.txt" });
             double y = ((Math.Pow(x, 3))/(Math.Pow(x, 2) - 1));
             y = Math.Round(y, 3);
             File.WriteAllText(path, Convert.ToString(y));
