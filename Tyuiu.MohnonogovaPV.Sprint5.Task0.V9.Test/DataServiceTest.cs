b60a401db058206d49d38cd934a2fe40e3b85113
Tyuiu.MohnonogovaPV@outlook.com <Tyuiu.MohnonogovaPV@outlook.com>
2024-11-25 12:48:46 +0500

    Task0

--- Tyuiu.MohnonogovaPV.Sprint5.Task0.V9.Test/DataServiceTest.cs
+++ Tyuiu.MohnonogovaPV.Sprint5.Task0.V9.Test/DataServiceTest.cs
@@ -8,11 +8,11 @@ namespace Tyuiu.MohnonogovaPV.Sprint5.Task0.V9.Test
         [TestMethod]
         public void CheckExistsFile()
         {
-            string path = @"C:\Users\“»”\source\repos\Tyuiu.MohnonogovaPV.Sprint5\Tyuiu.MohnonogovaPV.Sprint5.Task0.V9\bin\Debug\net8.0\OutPutFileTask0.txt";
+            string path = @"C:\Users\Admin\source\repos\Tyuiu.MohnonogovaPV.Sprint5\Tyuiu.MohnonogovaPV.Sprint5.Task0.V9\bin\Debug\net8.0\OutPutFileTask0.txt";
 
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
-            bool wait = true;
+            bool wait = false;
             Assert.AreEqual(wait, fileExists);
         }
     }
