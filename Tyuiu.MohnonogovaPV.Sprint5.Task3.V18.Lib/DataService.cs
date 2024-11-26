a2aad11c3412c8652410fa3e05032c9d5123ac06
Tyuiu.MohnonogovaPV@outlook.com <Tyuiu.MohnonogovaPV@outlook.com>
2024-11-26 20:46:27 +0500

    Task3

---
+++ Tyuiu.MohnonogovaPV.Sprint5.Task3.V18.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.MohnonogovaPV.Sprint5.Task3.V18.Lib
+{
+    public class DataService : ISprint5Task3V18
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask3.bin" });
+            double z = 2.12 * Math.Pow(x, 3) + 1.05 * Math.Pow(x, 2) + (4.1 * x) * 2;
+            z = Math.Round(z, 3);
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+            {
+                writer.Write(BitConverter.GetBytes(z));
+            }
+
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.MohnonogovaPV.Sprint5.Task3.V18/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.MohnonogovaPV.Sprint5.Task3.V18.Lib;
+namespace Tyuiu.MohnonogovaPV.Sprint5.Task3.V18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Мохноногова П. В. | АСОиУБ - 24-1";
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                                        *");
+            Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                                              *");
+            Console.WriteLine("* Задание #3                                                                                       *");
+            Console.WriteLine("* Вариант #18                                                                                      *");
+            Console.WriteLine("* Выполнила: Мохноногова Полина Викторовна | АСОиУБ - 24-1                                         *");
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                         *");
+            Console.WriteLine("* Дано выражение вычислить его значение при х = 2, результат сохранить в бинарный файл и вывести   *");
+            Console.WriteLine("* его на консоль. Округлить до трех знаков после запятой                                           *");
+            Console.WriteLine("*                                                                                                  *");
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                 *");
+            Console.WriteLine("****************************************************************************************************");
+
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                       *");
+            Console.WriteLine("****************************************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
