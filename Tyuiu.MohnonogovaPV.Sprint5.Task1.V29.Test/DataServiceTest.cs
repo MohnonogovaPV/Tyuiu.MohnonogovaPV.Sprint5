e5f7350c4a6efce03cfe10901f465763d16da8fd
Tyuiu.MohnonogovaPV@outlook.com <Tyuiu.MohnonogovaPV@outlook.com>
2024-11-26 18:45:38 +0500

    Task1

---
+++ Tyuiu.MohnonogovaPV.Sprint5.Task1.V29.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+using System.IO;
+using Tyuiu.MohnonogovaPV.Sprint5.Task1.V29.Lib;
+namespace Tyuiu.MohnonogovaPV.Sprint5.Task1.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckExistsFile()
+        {
+            string path = @"C:\Users\“»”\source\repos\Tyuiu.MohnonogovaPV.Sprint5\Tyuiu.MohnonogovaPV.Sprint5.Task1.V29\bin\Debug\net8.0\OutPutFileTask1.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            bool wait = false;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.MohnonogovaPV.Sprint5.Task1.V29/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.MohnonogovaPV.Sprint5.Task1.V29.Lib;
+namespace Tyuiu.MohnonogovaPV.Sprint5.Task1.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startValue = -5;
+            int stopValue = 5;
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Мохноногова П. В. | АСОиУБ - 24-1";
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                                        *");
+            Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                                          *");
+            Console.WriteLine("* Задание #1                                                                                       *");
+            Console.WriteLine("* Вариант #29                                                                                      *");
+            Console.WriteLine("* Выполнила: Мохноногова Полина Викторовна | АСОиУБ - 24-1                                         *");
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                         *");
+            Console.WriteLine("* Дана функция на заданном диапазоне. Произвести проверку деления на 0. Результат сохранить в      *");
+            Console.WriteLine("* текстовый файл и вывести на консоль таблицу. Значения округлить до 2 знаков после запятой        *");
+            Console.WriteLine("*                                                                                                  *");
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                 *");
+            Console.WriteLine("****************************************************************************************************");
+
+            Console.WriteLine("startValue = " + startValue);
+            Console.WriteLine("stopValue = " + stopValue);
+
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                       *");
+            Console.WriteLine("****************************************************************************************************");
+
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
